c07a0a8b1dbabcc76d210536c19cb7d82f1d321b
heeinso <heeinso@snu.ac.kr>
2017-06-23 16:23:42 +0900

    Implement profile pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -17,3 +18,8 @@ def signup(request):
     return render(request, 'accounts/signup_form.html', {
             'form': form,
         })
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
